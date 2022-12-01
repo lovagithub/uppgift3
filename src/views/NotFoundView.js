49ef90fc63820f26a5561ec7897bbd56a72c1065
Lova Juhlin <lova.juhlin@outlook.com>
2022-12-01 21:50:52 +0100

    404

--- src/views/NotFoundView.js
+++ src/views/NotFoundView.js
@@ -1,13 +1,17 @@
-import React from 'react'
+
 import FooterSection from '../sections/FooterSection'
 import MainMenuSection from '../sections/MainMenuSection'
+import NotFoundImage from "../assets/images/404-error.png"
 
 const NotFoundView = () => {
     return (
         <>
          <MainMenuSection />
-         <div className='container d-flex justify-content-center align-items-center' style={{ height: "500px"}}>
-         <h1>404 - Page Not Found</h1>
+         <div className='container'>
+         <div className='d-flex justify-content-center align-items-center vh-100'>  
+         <img src={NotFoundImage} alt="404 - Page Not Found" style={{ height: "500px"}} />
+         <h1 className='ms-4' style={{ fontSize: "3rem"}}> The page you were looking för couldn't be found.</h1>
+         </div>
          </div>
          <div style={{ position: "fixed", bottom: 0}}>
          <FooterSection /> 
