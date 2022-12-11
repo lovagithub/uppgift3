23d3ea1965130507bb6424391a2b4dc8691ac5f8
Lova Juhlin <lova.juhlin@outlook.com>
2022-12-11 10:59:14 +0100

    fugerar med min API

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -3,6 +3,8 @@ import { useContext, useState } from 'react'
 import React, {createContext} from 'react'
 import { ProductItem } from '../models/ProductModels'
 
+// 'https://win22-webapi.azurewebsites.net/api/products'
+
 interface ProductProviderType {
     children: any 
 }
@@ -19,9 +21,9 @@ export const ProductContext = createContext<ProductContextType | null>(null)
 export const useProductContext = () => { return useContext(ProductContext)}
 
 const ProductProvider: React.FC<ProductProviderType> = ({children}) => {
-    const baseUrl:string = 'https://win22-webapi.azurewebsites.net/api/products'
-    const EMPY_PRODUCT: ProductItem = { articleNumber: '',name: '', category: '', price: 0, imageName: ''}
-    const [product, setProduct] = useState<ProductItem>(EMPY_PRODUCT)
+    const baseUrl:string = 'http://localhost:5000/api/products'
+    const EMPTY_PRODUCT: ProductItem = { articleNumber: '',name: '', category: '', price: 0, imageName: ''}
+    const [product, setProduct] = useState<ProductItem>(EMPTY_PRODUCT)
     const [products, setProducts] = useState<ProductItem[]>([]) 
     const [featured, setFeatured] = useState<ProductItem[]>([])
     
